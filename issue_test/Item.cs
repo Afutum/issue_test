b43ca88f8fa243422b24fc9cb64670f0e9fc6a62
S-JOBI23-052\student <231116@st.yoshida-g.ac.jp>
2024-08-30 09:44:43 +0900

    Itemクラスの作成 #2

---
+++ issue_test/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace issue_test
+{
+    internal class Item
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+
+        public void CreateItem(int id,string name, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+
+        public void ShowItems()
+        {
+            Console.WriteLine("アイテムid" +  id + "アイテム名" + name + "値段" + price);
+            Console.ReadLine();
+        }
+    }
+}
